c00fc01d8823d3efcbd76fb8a577500c34aa643f
abhijeetpalanki <ajpalanki@gmail.com>
2022-11-14 18:00:27 -0600

    setup outline and game board

--- App.js
+++ App.js
@@ -1,10 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, ImageBackground } from "react-native";
+import bg from "./assets/bg.png";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <ImageBackground source={bg} style={styles.bg} resizeMode="contain">
+        <View style={styles.map}>
+          <View style={styles.circle} />
+          <View style={styles.cross}>
+            <View style={styles.crossLine} />
+            <View style={[styles.crossLine, styles.crossLineReversed]} />
+          </View>
+        </View>
+      </ImageBackground>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +22,64 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#242d34",
+  },
+  bg: {
+    width: "100%",
+    height: "100%",
+    alignItems: "center",
+    justifyContent: "center",
+
+    paddingTop: 20,
+  },
+  map: {
+    borderWidth: 1,
+    borderColor: "white",
+    width: "80%",
+    aspectRatio: 1,
+  },
+  circle: {
+    position: "absolute",
+
+    left: 1 * 105,
+    top: 2 * 105,
+
+    width: 75,
+    height: 75,
+    borderRadius: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    margin: 10,
+
+    borderWidth: 10,
+    borderColor: "white",
+  },
+  cross: {
+    position: "absolute",
+    width: 75,
+    height: 75,
+  },
+  crossLine: {
+    position: "absolute",
+    left: 32.5,
+    width: 10,
+    height: 70,
+    backgroundColor: "white",
+    borderRadius: 5,
+    transform: [
+      {
+        rotate: "45deg",
+      },
+    ],
+  },
+  crossLineReversed: {
+    transform: [
+      {
+        rotate: "-45deg",
+      },
+    ],
   },
 });
